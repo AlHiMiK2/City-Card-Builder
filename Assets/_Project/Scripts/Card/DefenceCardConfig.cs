f65c3b8c4d435f15eb7d84fbc23fb2947baa7014
AlHiMiK2 <127976865+AlHiMiK2@users.noreply.github.com>
2025-05-06 23:50:35 +0400

    Push

--- Assets/_Project/Scripts/Card/DefenceCardConfig.cs
+++ Assets/_Project/Scripts/Card/DefenceCardConfig.cs
@@ -5,12 +5,12 @@ namespace _Project.Scripts.Card
     [CreateAssetMenu(fileName = "New Defence Card", menuName = "Create Defence Card", order = 0)]
     public class DefenceCardConfig : CardConfig
     {
-        [SerializeField] private int _startHealth;
+        [SerializeField] private int _health;
         [SerializeField] private int _earn;
         [SerializeField] private Mesh _mesh;
         [SerializeField] private Material _material;
 
-        public int StartHealth => _startHealth;
+        public int Health => _health;
         public int Earn => _earn;
         public Mesh Mesh => _mesh;
         public Material Material => _material;

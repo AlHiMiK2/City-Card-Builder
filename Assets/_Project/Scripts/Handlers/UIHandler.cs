1551616c27b33beb2435ee5be82634bc0125e0e1
AlHiMiK2 <127976865+AlHiMiK2@users.noreply.github.com>
2025-05-14 15:17:57 +0400

    push

--- Assets/_Project/Scripts/Handlers/UIHandler.cs
+++ Assets/_Project/Scripts/Handlers/UIHandler.cs
@@ -12,7 +12,7 @@ namespace _Project.Scripts.Handlers
         [SerializeField] private string _turnPostfix;
         [SerializeField] private TMP_Text _turnView;
 
-        private List<List<TMP_Text>> _healthBars = new ();
+        private List<List<TMP_Text>> _constructionDataViews = new ();
         private Camera _camera;
         
         public Canvas Canvas => _canvas;
@@ -31,25 +31,25 @@ namespace _Project.Scripts.Handlers
         
         public void AddConstructionDataView(Vector3 worldPosition, int ownerIndex)
         {
-            if (_healthBars.Count - 1 < ownerIndex)
+            if (_constructionDataViews.Count - 1 < ownerIndex)
             {
-                _healthBars.Add(new List<TMP_Text>());
+                _constructionDataViews.Add(new List<TMP_Text>());
             }
             
             Vector3 position = _camera.WorldToScreenPoint(worldPosition);
             var instance = Instantiate(_constructionDataViewPrefab, position, Quaternion.identity, transform);
-            _healthBars[ownerIndex].Add(instance);
+            _constructionDataViews[ownerIndex].Add(instance);
         }
 
         public void SetConstructionDataViewValue(int maxHealth, int health, int earn, int index, int ownerIndex)
         {
             if (health > 0)
             {
-                _healthBars[ownerIndex][index].text = $"{health}/{maxHealth}HP \n {earn} Earn";
+                _constructionDataViews[ownerIndex][index].text = $"{health}/{maxHealth}HP \n {earn} Earn";
             }
             else
             {
-                _healthBars[ownerIndex][index].text = $"";
+                _constructionDataViews[ownerIndex][index].text = "";
             }
         }
 
